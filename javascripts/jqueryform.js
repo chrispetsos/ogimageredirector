61c7e504f6d5710b655ebf1812ca67ccabaa7593
chrispetsos <chrispetsos@gmail.com>
2016-10-11 14:14:49 +0300

    Tweaked the form to my needs.

--- javascripts/jqueryform.js
+++ javascripts/jqueryform.js
@@ -1,32 +1,25 @@
 $(document).ready(function() {
 $("div#form1").append(
 // Creating Form Div and Adding <h2> and <p> Paragraph Tag in it.
-$("<h3/>").text("Contact Form"), $("<p/>").text("This is my form. Please fill it out. It's awesome!"), $("<form/>", {
-action: '#',
-method: '#'
+$("<h3/>").text("Create redirect link"), $("<form/>", {
+//action: '#',
+//method: '#'
 }).append(
 // Create <form> Tag and Appending in HTML Div form1.
 $("<input/>", {
 type: 'text',
-id: 'vname',
-name: 'name',
-placeholder: 'Your Name'
+id: 'vimageurl',
+name: 'imageurl',
+placeholder: 'Custom og:image URL'
 }), // Creating Input Element With Attribute.
 $("<input/>", {
 type: 'text',
-id: 'vemail',
-name: 'email',
-placeholder: 'Your Email'
-}), $("<textarea/>", {
-rows: '5px',
-cols: '27px',
-type: 'text',
-id: 'vmsg',
-name: 'msg',
-placeholder: 'Message'
+id: 'vredirectto',
+name: 'redirectto',
+placeholder: 'Linked page URL'
 }), $("<br/>"), $("<input/>", {
 type: 'submit',
 id: 'submit',
-value: 'Submit'
+value: 'Create link'
 })))
 });
