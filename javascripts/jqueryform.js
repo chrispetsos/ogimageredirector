fcd41e2e5cdd258c1cfb56685d149af1786542f2
chrispetsos <chrispetsos@gmail.com>
2016-10-11 14:21:10 +0300

    Disabling the default form submission.

--- javascripts/jqueryform.js
+++ javascripts/jqueryform.js
@@ -1,25 +1,40 @@
 $(document).ready(function() {
-$("div#form1").append(
-// Creating Form Div and Adding <h2> and <p> Paragraph Tag in it.
-$("<h3/>").text("Create redirect link"), $("<form/>", {
-//action: '#',
-//method: '#'
-}).append(
-// Create <form> Tag and Appending in HTML Div form1.
-$("<input/>", {
-type: 'text',
-id: 'vimageurl',
-name: 'imageurl',
-placeholder: 'Custom og:image URL'
-}), // Creating Input Element With Attribute.
-$("<input/>", {
-type: 'text',
-id: 'vredirectto',
-name: 'redirectto',
-placeholder: 'Linked page URL'
-}), $("<br/>"), $("<button/>", {
-type: 'button',
-id: 'submit',
-value: 'Create link'
-})))
+	$("div#form1").append(
+//			Creating Form Div and Adding <h2> and <p> Paragraph Tag in it.
+			$("<h3/>").text("Create redirect link"), $("<form/>", {
+//				action: '#',
+//				method: '#'
+			}).append(
+//					Create <form> Tag and Appending in HTML Div form1.
+					$("<input/>", {
+						type: 'text',
+						id: 'vimageurl',
+						name: 'imageurl',
+						placeholder: 'Custom og:image URL'
+					}), // Creating Input Element With Attribute.
+					$("<input/>", {
+						type: 'text',
+						id: 'vredirectto',
+						name: 'redirectto',
+						placeholder: 'Linked page URL'
+					}), $("<br/>"), $("<button/>", {
+						type: 'button',
+						id: 'submit',
+						value: 'Create link'
+					})))
+});
+
+$(document).ready(function() {
+	$("#submit").click(function(e) {
+		e.preventDefault();
+		/*var name = $("#name").val();
+		var email = $("#email").val();
+		var msg = $("#msg").val();
+		if (!(name == '' || email == '' || msg == '')) {
+		$("#submitdata").empty();
+		$("#submitdata").append("Name: " + name + "<br/>Email: " + email + "<br/>Message: " + msg);
+		} else {
+		alert("Please Fill All Fields.");
+		}*/
+	});
 });
